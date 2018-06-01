82f8fcc7368d80580d936b1f9493c8c3483836e2
FilipStojanovic <39311662+FilipStojanovic@users.noreply.github.com>
2018-06-01 13:40:37 -0400

    Update retrieveAPI.js

--- retrieveAPI.js
+++ retrieveAPI.js
@@ -1,5 +1,5 @@
-function addZero(num){
-     	var ret = num.toString();
+   function addZero(num){
+		var ret = num.toString();
    	if (num < 10){
   	    ret = "0" + num.toString();
         }
@@ -106,3 +106,44 @@ function addZero(num){
       fetchText('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + ticker
 	      + '&interval=1min&apikey=4IZG324QO46F99VH');
     }	  
+
+
+    function writeToFile(responseAsText){
+	var txtFile = "cache.json";
+	var file = new File(txtFile);
+	var str = JSON.stringify(responseAsText);
+
+	file.open("w"); // open file with write access
+	file.writeln("First line of text");
+	file.writeln("Second line of text " + str);
+	file.write(str);
+	file.close();
+
+	/// read from file
+
+	var txtFile = "c:/test.txt"
+	var file = new File(txtFile);
+
+	file.open("r"); // open file with read access
+	var str = "";
+	while (!file.eof) {
+		// read each line of text
+		str += file.readln() + "\n";
+	}
+	file.close();
+	alert(str);
+	    
+    }
+
+////////////////////////////////////////////////////
+/**
+ * writeTextFile write data to file on hard drive
+ * @param  string  filepath   Path to file on hard drive
+ * @param  sring   output     Data to be written
+ */
+function writeTextFile(filepath, output) {
+	var txtFile = new File(filepath);
+	txtFile.open("w"); //
+	txtFile.writeln(output);
+	txtFile.close();
+}
